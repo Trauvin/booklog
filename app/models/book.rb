fa958ae3b2fc6a0da41233469eba38eb0d5f48a2
Trauvin <mateusgalote@gmail.com>
2020-09-23 13:25:30 -0300

    adjust interface

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,4 @@
 class Book < ApplicationRecord
-
-    def percentage
-        return (book.pages * 1) / (book.pages_read * 100)
-    end
+    validates :title, :author, :pages, :pages_read, presence: true
+    
 end
