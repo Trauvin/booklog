761d9370e6c9336d8fdb698158e81565d21334d8
Trauvin <mateusgalote@gmail.com>
2020-09-24 08:23:45 -0300

    more adjust

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def flash_class(level)
+        case level
+        when :notice then "has-text-link"
+        when :success then "has-text-success"
+        when :error then "has-text-danger"
+        when :alert then "has-text-warning"
+        end
+    end
 end
